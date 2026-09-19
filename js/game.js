77fbc8eba94a4de31e28d1fc1c3c3aca626033da
prabhjotsaini132006-lang <prabhjotsaini132006@gmail.com>
2026-09-19 11:24:37 +0530

    Implemented Player Movement

--- js/game.js
+++ js/game.js
@@ -5,8 +5,39 @@ const player = {
     x: 375,
     y: 375,
     width: 50,
-    height: 50
+    height: 50,
+    speed: 5
 };
+
+const keys={}
+
+window.addEventListener("keydown", (event) => {
+    keys[event.key.toLowerCase()] = true;
+});
+
+window.addEventListener("keyup", (event) => {
+    keys[event.key.toLowerCase()] = false;
+});
+
+function updatePlayer(){
+    if(keys["w"] || keys["arrowup"]){
+        player.y -= player.speed;
+    }
+
+    if(keys["s"] || keys["arrowdown"]){
+        player.y += player.speed;
+    }
+
+    if(keys["a"] || keys["arrowleft"]){
+        player.x -= player.speed;
+    }
+
+    if(keys["d"] || keys["arrowright"]){
+        player.x += player.speed;
+    }
+}
+
+  
 
 function drawPlayer(){
     ctx.fillStyle = "blue";
@@ -19,12 +50,16 @@ function drawPlayer(){
     );
 }
 
-function draw(){
-    ctx.clearRect(0,0, canvas.width, canvas.height);
+function gameLoop(){
+    updatePlayer();
+
+    ctx.clearRect(
+        0,0,canvas.width,canvas.height
+    );
 
     drawPlayer();
+
+    requestAnimationFrame(gameLoop);
 }
 
-draw();
-
-console.log("Player Created!");
+gameLoop();
