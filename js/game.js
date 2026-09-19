6101000aef03d577cc1ac0e0c4f9f6850efc3e6f
prabhjotsaini132006-lang <prabhjotsaini132006@gmail.com>
2026-09-19 12:07:32 +0530

    Add Dungeon Wall Collision

--- js/game.js
+++ js/game.js
@@ -19,21 +19,54 @@ window.addEventListener("keyup", (event) => {
     keys[event.key.toLowerCase()] = false;
 });
 
+function isColliding(rect1, rect2) {
+    return (
+        rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.y + rect1.height > rect2.y
+    );
+}
+
 function updatePlayer(){
     if(keys["w"] || keys["arrowup"]){
         player.y -= player.speed;
+
+        for(const wall of walls){
+            if(isColliding(player,wall)) {
+                player.y +=player.speed;
+            }
+        }
     }
 
     if(keys["s"] || keys["arrowdown"]){
         player.y += player.speed;
+
+        for(const wall of walls){
+            if(isColliding(player,wall)) {
+                player.y -=player.speed;
+            }
+        }
     }
 
     if(keys["a"] || keys["arrowleft"]){
         player.x -= player.speed;
+
+        for(const wall of walls){
+            if(isColliding(player,wall)){
+                player.x += player.speed;
+            }
+        }
     }
 
     if(keys["d"] || keys["arrowright"]){
         player.x += player.speed;
+
+        for(const wall of walls){
+            if(isColliding(player,wall)){
+                player.x -= player.speed;
+            }
+        }
     }
 
     if(player.x < 0){
@@ -54,7 +87,28 @@ function updatePlayer(){
 
 }
 
-  
+
+const walls = [
+    { x: 100, y: 100, width: 200, height: 30 },
+    { x: 500, y: 100, width: 200, height: 30 },
+    { x: 100, y: 470, width: 200, height: 30 },
+    { x: 500, y: 470, width: 200, height: 30 },
+    { x: 100, y: 130, width: 30, height: 340 },
+    { x: 670, y: 130, width: 30, height: 340 }
+];
+
+function drawWalls(){
+    ctx.fillStyle = "#555";
+
+    for (const wall of walls) {
+        ctx.fillRect(
+            wall.x,
+            wall.y,
+            wall.width,
+            wall.height
+        );
+    }
+}
 
 function drawPlayer(){
     ctx.fillStyle = "blue";
@@ -67,14 +121,17 @@ function drawPlayer(){
     );
 }
 
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawWalls();
+    drawPlayer();
+}
+
 function gameLoop(){
     updatePlayer();
 
-    ctx.clearRect(
-        0,0,canvas.width,canvas.height
-    );
-
-    drawPlayer();
+    draw();
 
     requestAnimationFrame(gameLoop);
 }
