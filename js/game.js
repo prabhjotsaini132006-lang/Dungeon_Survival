d4bdb2a2afdf91a306e437e5ac70cf35c85cea4f
prabhjotsaini132006-lang <prabhjotsaini132006@gmail.com>
2026-09-19 11:10:41 +0530

    feat: add player rendering

--- js/game.js
+++ js/game.js
@@ -1,4 +1,30 @@
 const canvas= document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-console.log("Dungeon Survival started!");
+const player = {
+    x: 375,
+    y: 375,
+    width: 50,
+    height: 50
+};
+
+function drawPlayer(){
+    ctx.fillStyle = "blue";
+
+    ctx.fillRect(
+        player.x,
+        player.y,
+        player.width,
+        player.height
+    );
+}
+
+function draw(){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+
+    drawPlayer();
+}
+
+draw();
+
+console.log("Player Created!");
